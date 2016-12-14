0985eae593da18b8f107320ed96c6108fddaa6f0
David Janes <davidjanes@davidjanes.com>
2016-12-14 10:52:55 -0500

    alexa authorization

--- web/server-dev.js
+++ web/server-dev.js
@@ -3,6 +3,15 @@ import express from 'express';
 import webpack from 'webpack';
 import config from './webpack.config.dev';
 
+const jwt = require("jsonwebtoken");
+const project_config = require("../config.json");
+
+const admin = require("firebase-admin");
+const admin_app = admin.initializeApp({
+    credential: admin.credential.cert("../firebase-admin.json"),
+    databaseURL: project_config.firebase.databaseURL,
+})
+
 const app = express();
 const compiler = webpack(config);
 
@@ -17,6 +26,59 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.use(express.static('static'));
+
+/**
+ *  The "original_token" is a short-lived JWT provided  by firebase. 
+ *  The "final_token" is a long-lived JWT that we've signed ourselves
+ *  and will be passed back to alexa for session management.
+ */
+app.use("/authorize-commit", (request, response) => {
+    const original_token = request.query.token;
+	if (!original_token) {
+		return response.send("ERROR: expected: token")
+	}
+
+    const client_id = request.query.client_id;
+	if (!client_id) {
+		return response.send("ERROR: expected: client_id")
+	} else if (client_id !== project_config.alexa.client_id) {
+		return response.send(`ERROR: expected client_id to be ${project_config.alexa.client_id}`);
+    }
+
+    const response_type = request.query.response_type;
+	if (!response_type) {
+		return response.send("ERROR: expected: response_type")
+	} else if (response_type !== "token") {
+		return response.send("ERROR: expected response_type to be 'token'")
+	}
+
+    const state = request.query.state;
+	if (!state) {
+		return response.send("ERROR: expected: state")
+	}
+
+    admin.auth()
+        .verifyIdToken(original_token)
+        .then(decoded_token => {
+            jwt.sign({ "uid": decoded_token.uid }, project_config.jwtSecret, {}, (error, final_token) => {
+                if (error) {
+                    return response.send(`ERROR: could not make JWT: ${error.message}`)
+                }
+
+                const return_url =
+                    project_config.alexa.redirect +
+                    "#state=" + state +
+                    "&access_token=" + final_token +
+                    "&token_type=Bearer";
+
+                return response.redirect(return_url);
+                // return response.send(`OK: ${final_token}`)
+            })
+        })
+        .catch(error => {
+            return response.send(`ERROR: could not verify token: ${error.message}`)
+        });
+})
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'static', 'index.html'));
--- web/src/components/AlexaAuthorize.jsx
+++ web/src/components/AlexaAuthorize.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 import C from '../constants';
+import * as firebase from 'firebase';
 
 import { listenToAuth } from '../actions/auth';
 import store from '../store'
@@ -12,14 +14,28 @@ import Header from './Header';
 class AlexaAuthorize extends React.Component {
 	constructor() {
 		super();
+
+		this.authorize = this.authorize.bind(this);
 	};
 
 	componentWillMount() {
         store.dispatch(listenToAuth(false));
-	}
+	};
+
+	authorize() {
+		const props = this.props;
+		const query = this.props.location.query;
+
+		firebase.auth().currentUser.getToken(true).then(token => {
+			window.location = `/authorize-commit?token=${token}&client_id=${query.client_id}&response_type=${query.response_type}&state=${query.state}`;
+		}).catch(error => {
+			alert(`something went wrong: ${error.message}`)
+		});
+	};
 
 	render() {
 		const props = this.props;
+		const query = this.props.location.query;
 
 		return (
 			<div>
@@ -40,14 +56,20 @@ class AlexaAuthorize extends React.Component {
 
 			<tr>
 			<td>client_id</td>
-			<td>{props.params.client_id}</td>
+			<td>{query.client_id}</td>
 			<td>alexa-opendata</td>
 			</tr>
 
 			<tr>
 			<td>token</td>
-			<td>{props.params.client_id}</td>
+			<td>{query.response_type}</td>
 			<td>token</td>
+			</tr>
+
+			<tr>
+			<td>state</td>
+			<td>{query.state}</td>
+			<td>(some value)</td>
 			</tr>
 
 			<tr>
@@ -58,6 +80,9 @@ class AlexaAuthorize extends React.Component {
 
 			</tbody>
 			</table>
+
+
+			<button className="btn" onClick={() => this.authorize()}>Authorize</button>
 
 
 			</div>
